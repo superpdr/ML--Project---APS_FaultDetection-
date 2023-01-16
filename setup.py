58cac3b6a96b92c68f9399c5bdd2cf225b03ddb5
Piyush <piyushraipure2007@gmail.com>
2023-01-16 11:36:41 +0530

    updated requirement and created setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+
+def get_rewuirements():
+    pass
+
+
+
+
+
+
+setup(
+        name="sensor",
+        version="0.0.1",
+        author = "Piyush",
+        author_email = "roadtodsmlai@gmail.com",
+        packages= find_packages(),
+        install_requires = get_requirements(),
+
+)
