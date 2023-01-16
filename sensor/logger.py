b56e33a77929c1e30ed83298a3f20a2727a132a8
piyush <piyushraipure2007@gmail.com>
2023-01-16 16:17:55 +0530

    updated error removed from logger.py

--- sensor/logger.py
+++ sensor/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 #create the log file 
-LOG_FILE_NAME = f"{datetime.now().strftime('%M%d%y____%H%M%S')}.log"
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
 
 #create the log directory 
 LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
@@ -15,8 +15,8 @@ os.makedirs(LOG_FILE_DIR,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 
-logging.basicConf(
-    filename = LOG_FILE_PATH,
-    format = "[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"
-    level = logging.INFO
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
